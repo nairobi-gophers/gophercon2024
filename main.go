3395b18b04b18014e183901ac9a1860faef1a670
Njoguu <njoguuallan@gmail.com>
2024-01-06 19:52:47 +0300

    add endpoint to test build works

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import(
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+
+var Server *gin.Engine
+
+func init(){
+
+	Server = gin.Default() //router
+
+}
+
+func main(){
+
+	// Test API works
+	Server.GET("/api/ping", func(c *gin.Context){
+		c.IndentedJSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	Server.Run() // listen and serve on 0.0.0.0:8080
+}
