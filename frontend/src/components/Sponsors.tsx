efba452d0baa66aed63d06e94f98b2a11774f0cd
nutcas3 <mauricee423@gmail.com>
2024-07-01 04:22:12 +0300

    revert

--- frontend/src/components/Sponsors.tsx
+++ frontend/src/components/Sponsors.tsx
@@ -1,11 +1,11 @@
-// src/Sponsors.tsx
-export default function Sponsors() {
-  const handleLinkClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
-    event.preventDefault();
+import React from 'react';
 
-    window.open('/Gophercon Africa 2024 Sponsorship Prospectus.pdf', '_blank');
+export default function Sponsors() {
+  const handleButtonClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    event.preventDefault(); // Prevents the default behavior of the anchor tag (navigation)
 
   };
+
   return (
     <section
       id="sponsors"
@@ -15,16 +15,21 @@ export default function Sponsors() {
         Sponsor The Event
       </h2>
       <p className="max-w-lg mb-8 text-sm md:text-base">
-          Don't miss this chance to supercharge your brand and tap into the vibrant energy of Nairobi's Go programming community. Contact us today to explore more sponsorship opportunities and join us in creating an unforgettable event.</p>
+        Don't miss this chance to supercharge your brand and tap into the vibrant energy of Nairobi's Go programming community. Contact us today to explore more sponsorship opportunities and join us in creating an unforgettable event.
+      </p>
 
       <div className="flex flex-col md:flex-row gap-4">
         <a
           className="button w-full md:w-auto mb-4 md:mb-0"
-          onClick={handleLinkClick}
+          href="/sponsorship"
+          onClick={handleButtonClick} // Call the function to handle button click
         >
           Sponsorship Details
         </a>
-        <a className="button w-full md:w-auto" href="mailto:hello@gophers.africa?subject=GopherCon%20Africa%20Sponsorship">
+        <a
+          className="button w-full md:w-auto"
+          href="mailto:hello@gophers.africa?subject=GopherCon%20Africa%20Sponsorship"
+        >
           Become a Sponsor
         </a>
       </div>
