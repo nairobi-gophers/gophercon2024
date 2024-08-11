fcc263a07b663120cc9616909a75dbb7b9403a2f
David Odohi <odohi.david@gmail.com>
2024-08-11 18:34:53 +0100

    add group of five ticket and update the ticket links to paystack link

--- frontend/src/components/Ticket.tsx
+++ frontend/src/components/Ticket.tsx
@@ -6,7 +6,7 @@ function SponsorCard(props: { title: string, price: string, body: string, isPopu
   return (
     <div className={`p-6 rounded-lg  border text-center flex flex-col gap-6 h-[350px] justify-center relative ${isPopular ? ` bg-primary text-white border-white` : ' border-secondary mt-6 '}`}>
       {isPopular ? <div className="bg-white text-primary px-2 py-1 w-fit text-xs absolute top-3 left-3 rounded-3xl font-semibold">Popular</div> : null}
-      <h3 className="">{title}</h3>
+      <h3 className="text-lg">{title}</h3>
       <p className="font-bold text-3xl">{price}</p>
       <p className="text-sm text-pretty md:w-[300px]">{body}</p>
       <Link
@@ -35,21 +35,30 @@ const Ticket = () => {
           title="Student Ticket"
           price="1000 KSH"
           body='In a bid to encourage students we have a package specific for you.'
-          link='https://appslab-qcpjjn.shop.chpter.co/product/481cc1c2-9793-4d97-9432-f04521537be8'
+          link='https://paystack.com/buy/student-ticket-qvbzec'
           isPopular={false}
         />
         <SponsorCard
           title="Regular Ticket"
           price="2000 KSH"
           body='This package is for our veteran gophers who want to join us for our 2 day thrilling Gophercon event'
-          link='https://appslab-qcpjjn.shop.chpter.co/product/bd9e697f-fb0b-4c1a-9fbc-d3d606e69487'
+          link='https://paystack.com/buy/regular-ticket-pjeyye'
           isPopular={true}
         />
         <SponsorCard
           title="Corporate Ticket"
           price="3500 KSH"
           body='This conference ticket is designated for Corporate Gophers and is available for purchase as a single ticket or in groups of five.'
-          link='https://appslab-qcpjjn.shop.chpter.co/product/3ccfbd3d-49bc-41d0-8b5c-4c2f3df71a0d'
+          link='https://paystack.com/buy/corporate-ticket-znbmom'
+          isPopular={false}
+        />
+      </div>
+      <div className="flex flex-col lg:flex-row gap-6 justify-center">
+        <SponsorCard
+          title="Group of 5"
+          price="8500 KSH"
+          body='This package is for a group of five people who want to join us for our 2 day thrilling Gophercon event.'
+          link='https://paystack.com/buy/group-of-five-15-off-wjvosb'
           isPopular={false}
         />
       </div>
