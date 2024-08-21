c34efe1dd88a993e3b5c11a128d7278825957c67
nutcas3 <mauricee423@gmail.com>
2024-08-21 20:38:50 +0300

    Fix Router Configuration and Page Imports
        Router Setup:
            Configured routes using createBrowserRouter for the React application.
            Added routes for Home, SponsorshipRedirect, Sponsors, SpeakersList, and TicketList pages.
    
        Resolved Duplicate Path:
            Removed duplicate /sponsors path to avoid routing conflicts.
            Ensured correct mapping of components for /sponsorship and /sponsors.
    
        Component Imports:
            Imported necessary components and pages: Home, Error, SponsorshipRedirect, Sponsors, Sponsorship, SpeakersList, and TicketList.
    
        Rendering:
            Set up RouterProvider with the configured router in the index.tsx file for application routing.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Error from "./pages/Error.tsx";
-import SponsorshipRedirect from './pages/SponsorshipRedirect'; 
+import SponsorshipRedirect from './pages/SponsorshipRedirect';
+import Sponsors from './components/Sponsors.tsx';
+import Sponsorship from './pages/Sponsorship.tsx';
+import SpeakersList from './pages/SpeakersList.tsx';
+import TicketList from './pages/TicketsList.tsx';
 
 
 const router = createBrowserRouter([
@@ -15,8 +19,24 @@ const router = createBrowserRouter([
   },
   {
     path: "/sponsorship",
-    element: <SponsorshipRedirect />, 
+    element: <SponsorshipRedirect />,
   },
+  {
+    path: "/sponsors",
+    element: <Sponsorship />,
+  },
+  {
+    path: "/sponsors",
+    element: <Sponsors />,
+  },
+  {
+    path: "/speakers",
+    element: <SpeakersList />,
+  },
+  {
+    path: "/tickets",
+    element: <TicketList />,
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
