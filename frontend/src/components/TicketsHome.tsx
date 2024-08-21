e9b803080b4d588680d4d676947709a47d7e0100
nutcas3 <mauricee423@gmail.com>
2024-08-21 21:07:26 +0300

    made navbar to be on active underlined

--- frontend/src/components/TicketsHome.tsx
+++ frontend/src/components/TicketsHome.tsx
@@ -12,32 +12,8 @@ export default function TicketHome() {
         Our Event Sponsors
       </h2>
       <p className="max-w-lg mb-8 text-sm md:text-base">
-        Don't miss this chance to supercharge your brand and tap into the vibrant energy of Nairobi's Go programming community. Connect with over 100 senior Go developers and contact us today to explore more sponsorship opportunities. Join us in creating an unforgettable event!
-      </p>
+      Buy your tickets to GopherCon Africa 2024 and join hundreds of leading developers from across the Go community for 2 days of talks, networking and socials.      </p>
 
-      <div className="flex flex-col md:flex-row gap-4 md-6">
-        <a
-          className="button w-full md:w-auto mb-4 md:mb-0"
-          href="/sponsorship"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Sponsorship Details
-        </a>
-        <a
-          className="button w-full md:w-auto"
-          href="mailto:hello@gophers.africa?subject=GopherCon%20Africa%20Sponsorship"
-        >
-          Become a Sponsor
-        </a>
-      </div>
-      <div className="p-6 max-w-lg mx-auto ">
-        <div className="flex items-center justify-between mt-20">
-          <div>
-            <h2 className="text-2xl font-bold text-black">Our Sponsors</h2>
-          </div>
-        </div>
-      </div>
       <Ticket />
 
 
