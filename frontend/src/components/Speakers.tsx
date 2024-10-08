bfb02f44ebb6e6a64ccd6bd2637dfb7c1d1a46cf
nutcas3 <mauricee423@gmail.com>
2024-10-08 15:14:12 +0300

    fixed social being obstructed and the speakers design to match the other pages

--- frontend/src/components/Speakers.tsx
+++ frontend/src/components/Speakers.tsx
@@ -3,16 +3,15 @@ import SpeakersList from "./SpeakersLists";
 export default function Speakers() {
     return (
         <section
-            id="sponsors"
-            className="container mx-auto px-6 py-12 md:py-24 flex flex-col items-center text-center text-secondary mt-20"
+            id="speakers"
+            className="container mx-auto py-12 md:py-24 flex flex-col items-center text-center text-secondary"
         >
-            <h2 className="text-3xl md:text-5xl font-bold mt-8 mb-6">
-                Our Event Speakers
+            <h2 className="text-3xl md:text-5xl font-bold mb-8 mt-8">
+                Our Speakers
             </h2>
-            <p className="max-w-lg mb-8 text-sm md:text-base">
+            <p className="w-full lg:w-[60%] text-sm md:text-base">
                 GopherCon Africa features global speakers who deliver cutting-edge content on Go programming and related technologies to keep attendees at the forefront of the field.
             </p>
-
             <div className="p-6 max-w-lg mx-auto ">
                 <div className="flex items-center justify-between mt-20">
                     <div>
