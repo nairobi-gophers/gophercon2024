cbedc446b1f3de3c6fa3baf0f39508c509f13c30
nutcas3 <mauricee423@gmail.com>
2024-10-17 22:32:12 +0300

    added es6 module to fix the error

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,4 @@
-/* eslint-disable no-undef */
-/* eslint-disable @typescript-eslint/no-var-requires */
-const plugin = require('tailwindcss/plugin')
+import plugin from 'tailwindcss/plugin';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -10,12 +8,12 @@ export default {
   ],
   theme: {
     extend: {
-        textShadow: {
-            sm: '0 1px 2px var(--tw-shadow-color)',
-            DEFAULT: '0 2px 4px var(--tw-shadow-color)',
-            lg: '0 8px 6px var(--tw-shadow-color)',
-        },
-       colors: {
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 6px var(--tw-shadow-color)',
+      },
+      colors: {
         primary: "#0055E9",
         secondary: "#0A0047",
         accent: "#EDF6FF",
@@ -23,16 +21,15 @@ export default {
     },
   },
   plugins: [
-      plugin(function ({ matchUtilities, theme }) {
-          matchUtilities(
-              {
-                  'text-shadow': (value) => ({
-                      textShadow: value,
-                  }),
-              },
-              { values: theme('textShadow') }
-          )
-      }),
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      );
+    }),
   ],
-}
-
+};
