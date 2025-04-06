5876e8acdaede7bee3811642c03536bdfe649266
David Odohi <odohi.david@gmail.com>
2025-04-06 17:16:36 +0100

    update date

--- frontend/src/components/HomeBanner.tsx
+++ frontend/src/components/HomeBanner.tsx
@@ -3,11 +3,11 @@ export function HomeBanner() {
     <>
       <h1 className="text-5xl lg:text-7xl font-bold text-primary">Gophercon Africa</h1>
       <p className="lg:w-[600px]  text-pretty mx-auto">
-        Africa's first Gophercon arrives! Join local & international Go enthusiasts, experts, and rising stars for an unforgettable celebration of the Go programming language. Grab your ticket or become a sponsor - let's make history together!
+        Join local & international Go enthusiasts, experts, and rising stars for an unforgettable celebration of the Go programming language. Grab your ticket or become a sponsor - let's make history together!
       </p>
 
       <div className="w-fit p-8 space-y-2 mx-auto border border-secondary rounded-md bg-white">
-        <p><b>Date:</b> 18th - 19th October 2024</p>
+        <p><b>Date:</b> 17th - 18th October 2025</p>
         <p><b>City:</b> Nairobi, Kenya</p>
         <p><b>Venue:</b> iHub</p>
         <div>
